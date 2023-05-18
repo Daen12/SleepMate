7b24afd45474d1eedd41c8516d9228d918595330
bigstar017 <335005@naver.com>
2023-05-18 17:03:33 +0900

    BackEnd init commit
    BackEnd init commit

---
+++ Backend/src/main/java/com/ssafy/YogaMate/YogaMateApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.YogaMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaMateApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ssafy/YogaMate/YogaMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.YogaMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
