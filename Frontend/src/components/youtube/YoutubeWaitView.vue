b60ca25cd82dc6403444522c0601e7f0a2494ca8
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-05-24 01:44:34 +0900

    youtube view made

---
+++ Frontend/src/components/youtube/YoutubeWaitView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="youtubes">
+        
+        <div v-if="!loginUser" class="desc">
+          로딩중입니다. 잠시만 기다려주세요...
+        </div>
+
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.youtubes .desc {
+  /* padding-left: 30px;
+  margin-top: 5px;
+  margin-bottom: 30px; */
+  margin-top: 200px;
+  text-align: center;
+  margin: auto;
+  font-size: 30px;
+  color: #ffffffcb;
+}
+</style>
