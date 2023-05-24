05fc63e13242683029d44ed5c12c4aa37a85f0f3
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-05-24 09:04:34 +0900

    loading

--- Frontend/src/components/youtube/YoutubeWaitView.vue
+++ Frontend/src/components/youtube/YoutubeWaitView.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="youtubes">
-        
-        <div v-if="!loginUser" class="desc">
+        <div class="desc">
           로딩중입니다. 잠시만 기다려주세요...
+            <img class="loading" src="images/loading.gif" alt="">
         </div>
-
       </div>
 </template>
 
@@ -22,7 +21,14 @@ export default {
   margin-top: 200px;
   text-align: center;
   margin: auto;
-  font-size: 30px;
+  font-size: 25px;
   color: #ffffffcb;
+}
+
+.loading {
+    /* margin-top: -10px; */
+    height: 70px;
+    width: 150px;
+    margin-left: -30px;
 }
 </style>
